ff43176def823a1e39532016ecdfcf3c68cfb0f1
Sergiy Blednov <serg.blednov@gmail.com>
2021-06-21 17:26:22 -0400

    Lecture 9. Add-on

---
+++ EmojiArt/EmojiArtModel.Background.swift
@@ -0,0 +1,30 @@
+//
+//  Background.swift
+//  EmojiArt
+//
+//  Created by Sergey Blednov on 6/21/21.
+//
+
+import Foundation
+
+extension EmojiArtModel {
+    enum Background {
+        case blank
+        case url(URL)
+        case imageData(Data)
+        
+        var url: URL? {
+            switch self {
+            case .url(let url): return url
+            default: return nil
+            }
+        }
+        
+        var imageData: Data? {
+            switch self {
+            case .imageData(let data): return data
+            default: return nil
+            }
+        }
+    }
+}
